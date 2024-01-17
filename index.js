f99fd764d11602b258b295f988d72eedcecf6824
Swaraj-singh-1255 <swarajsinghrajput1255@gmail.com>
2024-01-17 22:11:10 +0530

    Netflix clone
    Front end part completed .

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const playBtn = document.querySelector(".visually-hidden");
+const label = document.querySelector(".label");
+const audio = document.querySelector(".audio");
+
+playBtn.addEventListener("click", function (e) {
+	if (e.target.checked) {
+		audio.play();
+		label.innerHTML = "pause";
+		label.classList.remove("move");
+		label.classList.add("back");
+	} else {
+		audio.pause();
+		label.innerHTML = "play";
+		label.classList.add("move");
+		label.classList.remove("back");
+	}
+});
